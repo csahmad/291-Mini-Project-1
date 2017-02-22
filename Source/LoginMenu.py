cc2f8ed13cd7bd811065dbfb305b72f8f2501a05
ahmad <ahmad@ualberta.ca>
2017-02-21 22:13:59 -0700

    Renamed LoginScreen to LoginMenu

---
+++ Source/LoginMenu.py
@@ -0,0 +1,6 @@
+from TerminalInterface import TerminalInterface
+
+class LoginMenu:
+	"""The login/signup menu for the terminal interface"""
+
+	pass
--- Source/LoginScreen.py
+++
@@ -1,6 +0,0 @@
-from TerminalInterface import TerminalInterface
-
-class LoginScreen:
-	"""The login screen for the terminal interface"""
-
-	pass
