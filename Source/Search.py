8e23f7867ff49a8dd1e272c69dae1d786bc5afab
Jessica Antonette Prieto <jessicaa@cs.ualberta.ca>
2017-02-22 14:41:47 -0700

    search tweets

--- Source/Search.py
+++ Source/Search.py
@@ -1,29 +1,40 @@
+import cx_Oracle
+
 class SearchTweet:
-	def search(cursor, tid):
-		"""Return whether the given string value exists in the given table"""
-	cursor.execute(
-		"select * from tweets where tid = {1}".format(tid))
+    def formatsearch (keyword):
+        result = '%{0}%'.format(keyword)
+        print(result)
+        return result
 
-	result = cursor.fetchone()
-	print("--------------------------------")
-	print("TWEET:")
-	print(result)
-	print("--------------------------------")
-
-	if result is None: return null
-	return result
+    def search(cursor, keyword):
+        """Return whether the given string value exists in the given table"""
+        fkeyword = SearchTweet.formatsearch(keyword)
+        cursor.execute("select tid, text from tweets where text like '{0}'".format(fkeyword))
+        result = cursor.fetchall()
+        print("--------------------------------")
+        for i in result:
+            print()
+            print("tweet id: %i" % i[0])
+            print()
+            print("tweet:")
+            print(i[1])
+            print()
+            print("--------------------------------")
+        return result
 
 # Interactive test
 if __name__ == "__main__":
+    from OracleTerminalConnection import OracleTerminalConnection
+    
+# Get connection to database and cursor
+    
+    f = open('.info')
+    username = f.readline().strip()
+    password = f.readline().strip()
+    connection = cx_Oracle.connect(username, password, "gwynne.cs.ualberta.ca:1521/CRS")
+    cursor = connection.cursor()
+    print()
+    keyword = input("enter keyword to search: ")
+    SearchTweet.search(cursor, keyword)
 
-	from OracleTerminalConnection import OracleTerminalConnection
-
-	# Get connection to database and cursor
-	connection = OracleTerminalConnection.connect()
-	cursor = connection.cursor()
-	print()
-
-	tid = input("enter tid to search: ")
-	SearchTweet.search(cursor, int(tid))
-
-	connection.close()
+    connection.close()
