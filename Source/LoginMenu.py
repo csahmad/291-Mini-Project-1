d3c3231d8ace1f891bfad4f53c8e05fee1cb14f9
ahmad <ahmad@ualberta.ca>
2017-02-22 10:20:17 -0700

    Fixed import error for UsersTableTools

--- Source/LoginMenu.py
+++ Source/LoginMenu.py
@@ -2,7 +2,7 @@ from TerminalMenu import TerminalMenu
 from TerminalForm import TerminalForm, FormField
 from TerminalInterface import TerminalInterface
 from IDGenerator import IDGenerator
-from UsersTableTools import UsersTableTools
+from TableTools import UsersTableTools
 
 class LoginMenu:
 	"""The login/signup menu for the terminal interface"""
