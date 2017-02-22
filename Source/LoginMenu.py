b421e5863d925f3d79ff1675912b516ed1c26c82
ahmad <ahmad@ualberta.ca>
2017-02-22 09:57:52 -0700

    Updated LoginMenu

--- Source/LoginMenu.py
+++ Source/LoginMenu.py
@@ -97,4 +97,12 @@ class LoginMenu:
 
 # Interactive test
 if __name__ == "__main__":
-	pass
+
+	# Get connection to database and cursor
+	connection = OracleTerminalConnection.connect()
+	cursor = connection.cursor()
+
+	user = LoginMenu.getUser(cursor)
+	print(user)
+
+	connection.close()
