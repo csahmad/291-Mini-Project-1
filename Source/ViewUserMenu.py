23b6250bf66c901e9082274ee1c453ad9706504a
ahmad <ahmad@ualberta.ca>
2017-02-24 10:19:42 -0700

    Added ViewUserMenu

---
+++ Source/ViewUserMenu.py
@@ -0,0 +1,15 @@
+class ViewUserMenu:
+	"""A menu for viewing a user"""
+
+	def __init__(self, loginID, userID):
+		"""
+		Arguments:
+		loginID -- the ID of the signed in user
+		userID -- the ID of the user to view
+		"""
+
+		pass
+
+	def showAndGet(self):
+
+		pass
