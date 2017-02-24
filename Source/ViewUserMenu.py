3a714f86993c78acafc2f0698ca328ba824a4e03
ahmad <ahmad@ualberta.ca>
2017-02-24 11:53:58 -0700

    Filled out ViewUserMenu

--- Source/ViewUserMenu.py
+++ Source/ViewUserMenu.py
@@ -1,5 +1,18 @@
+from TerminalMenu import TerminalMenu
+from TableTools import UsersTableTools, FollowsTableTools
+from UserTweetsMenu import UserTweetsMenu
+from DateTools import DateTools
+
 class ViewUserMenu:
 	"""A menu for viewing a user"""
+
+	_FOLLOW_INDEX = 0
+	_VIEW_TWEETS_INDEX = 1
+	BACK_INDEX = 2
+	_INITIAL_INDEX = -1
+
+	_OPTIONS = ["Follow", "View tweets", "Back"]
+	_OPTIONS_WITHOUT_FOLLOW = ["View tweets", "Back"]
 
 	def __init__(self, cursor, loginID, userID):
 		"""
@@ -11,7 +24,58 @@ class ViewUserMenu:
 		self._cursor = cursor
 		self._loginID = loginID
 		self._userID = userID
+		self._userStats = UsersTableTools.getUserStats(cursor, userID)
+		self._isFollowing = FollowsTableTools.isFollowing(cursor, loginID,
+			userID)
 
 	def showAndGet(self):
+		"""
+		Show the menu and return either ViewUserMenu.BACK_INDEX (if back
+		option chosen) or None (if an exit key was pressed)
+		"""
 
-		pass
+		choice = ViewUserMenu._INITIAL_INDEX
+
+		while choice is not None and choice != ViewUserMenu.BACK_INDEX:
+			choice = self._showAndGet
+
+		return choice
+
+	def _showAndGet(self):
+		"""
+		Show the menu and return either ViewUserMenu.BACK_INDEX (if back
+		option chosen) or None (if an exit key was pressed)
+		"""
+
+		preMessage = self._userID + "\n\t" + self._userStats
+
+		if self._isFollowing:
+			preMessage = "You follow this user" + preMessage
+			options = _OPTIONS_WITHOUT_FOLLOW
+
+		else:
+			options = _OPTIONS
+
+		menu = TerminalMenu(options, preMessage)
+		choice = menu.showAndGet()
+
+		if self._isFollowing: choice += 1
+
+		# If an exit key was pressed, return None
+		if choice is None: return None
+
+		# If user chose to go back, return ViewUserMenu.BACK_INDEX
+		if choice == ViewUserMenu.BACK_INDEX:
+			return ViewUserMenu.BACK_INDEX
+
+		# If user chose to follow, follow
+		elif choice == ViewUserMenu._FOLLOW_INDEX:
+			date = DateTools.getCurrentDate()
+			FollowsTableTools.follow(self._cursor, self._loginID, self._userID,
+				date)
+			self._isFollowing = True
+
+		# If user chose to view tweets, view tweets
+		else:
+			tweetsMenu = UserTweetsMenu(self._cursor, self._loginID,
+				self._userID)
